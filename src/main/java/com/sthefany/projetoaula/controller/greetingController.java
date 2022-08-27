1553cf2d7177909f90612ab1c1e751e745ad2b2d
SthefanyLorenay <sthefany.souza@estudante.ifms.edu.br>
2022-08-26 21:30:41 -0400

    aula 26/08

--- src/main/java/com/sthefany/projetoaula/controller/greetingController.java
+++ src/main/java/com/sthefany/projetoaula/controller/greetingController.java
@@ -1,8 +1,18 @@
 package com.sthefany.projetoaula.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sthefany.projetoaula.domain.Greeting;
+
 @RestController
-public class greetingController {
+@RequestMapping(value = "/greeting")
+public class GreetingController {
+
+
+    @GetMapping
+    public ResponseEntity <String> digaola (){
+        Greeting greeting = new Greeting();
+        return greeting.getSaudacao();    }
     
 }
