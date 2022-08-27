6fbbd4f7be78ad0f156e127ba779ea565dd9022b
SthefanyLorenay <sthefany.souza@estudante.ifms.edu.br>
2022-08-26 21:39:24 -0400

    importação dos métodos

--- src/main/java/com/sthefany/projetoaula/controller/greetingController.java
+++ src/main/java/com/sthefany/projetoaula/controller/greetingController.java
@@ -1,6 +1,8 @@
 package com.sthefany.projetoaula.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sthefany.projetoaula.domain.Greeting;
@@ -13,6 +15,6 @@ public class GreetingController {
     @GetMapping
     public ResponseEntity <String> digaola (){
         Greeting greeting = new Greeting();
-        return greeting.getSaudacao();    }
+        return ResponseEntity.ok(greeting.getSaudacao());    }
     
 }
